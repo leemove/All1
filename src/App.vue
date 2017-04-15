c50499980ae5a47af588f34f634cd9c2106e7407
Leemove <18686991620@163.com>
2017-04-15 11:20:10 +0800

    脚手架搭建,完善less编译

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <div>一些我想说的事情: <span>我想变个颜色</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scope lang='less'>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: hotpink;
+  margin-top: 60px;
+  div{
+    span{
+      color: black;
+    }
+  }
+}
+</style>
